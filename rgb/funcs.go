5b9d9532fc428ae8ea00f5345211f18b2378458e
Luke Thorne <the.thorne48@gmail.com>
2019-08-12 21:54:30 +0000

    Add old Golang implementation

--- rgb/funcs.go
+++ rgb/funcs.go
@@ -48,6 +48,14 @@ var LightDefaults = [...]uint8{
 	0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0}
 
+// SinAppend special function to build my SIN map slices
+func SinAppend(slice []uint8, value float64, maxBrightness float64) []uint8 {
+	if value >= 0 {
+		return append(slice, uint8(value*maxBrightness))
+	}
+	return append(slice, 0)
+}
+
 // Color creates a uint32 value of the RGB color
 func Color(red uint8, green uint8, blue uint8) uint32 {
 	const white uint8 = 0
@@ -58,4 +66,15 @@ func Color(red uint8, green uint8, blue uint8) uint32 {
 // To use, simply map your light strip to 360 degrees of colors.
 func RainbowColor(angle int) uint32 {
 	return Color(LightDefaults[(angle+len(LightDefaults)/3)%len(LightDefaults)], LightDefaults[angle%len(LightDefaults)], LightDefaults[(angle+(len(LightDefaults)/3)*2)%len(LightDefaults)])
+}
+
+// RainbowCosColor takes an angle and creates a color from it.
+// To use, simply map your light strip to 360 degrees of colors.
+func RainbowCosColor(floats RGB, seed int) uint32 {
+	maxIndex := len(floats.red)
+	// splitIndex := len(floats.red) / 3
+	red := floats.red[seed%maxIndex]
+	green := floats.green[seed%maxIndex]
+	blue := floats.blue[seed%maxIndex]
+	return Color(red, green, blue)
 }
--- rgb/rainbow.go
+++ rgb/rainbow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"time"
 
@@ -12,27 +13,102 @@ const (
 	pin        = 18
 	count      = 150
 	brightness = 100
-	maxAngle   = 255
+	maxAngle   = 360
 )
+
+// RGB - a set of arrays to hold pre-calculated RGB values
+type RGB struct {
+	red   []uint8
+	green []uint8
+	blue  []uint8
+}
 
 func main() {
 	defer ws2811.Fini()
+	colorValues := initRange(count)
 	err := ws2811.Init(pin, count, brightness)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println("Press Ctr-C to quit.")
+
+		fmt.Println("Creating cosine rainbow")
+		for index := 0; true; index++ {
+			err = rainbowCosCycle(colorValues, index)
+			if err != nil {
+				fmt.Println("Error during cycle " + err.Error())
+				os.Exit(-1)
+			}
+		}
+
+		fmt.Println("Creating color flash")
+		colors := [...]uint32{
+			0xFF0000, // green
+			0x888800, // yellow
+			0x00FF00, // red
+			0x00FFFF, // purple
+			0x0000FF, // blue
+			0xFF00FF, // cyan
+		}
+		for i := 0; i < len(colors); i++ {
+			err = colorFlash(colors[i])
+			if err != nil {
+				fmt.Println("Error during flash " + err.Error())
+				os.Exit(-1)
+			}
+		}
+
 		fmt.Println("Creating rainbow circle")
-		index := 0
-		for {
+
+		for index := 0; true; index++ {
 			err = rainbowCycle(index % maxAngle)
 			if err != nil {
 				fmt.Println("Error during wipe " + err.Error())
 				os.Exit(-1)
 			}
-			index++
 		}
 	}
+}
+
+func initRange(ledCount int) RGB {
+	floats := RGB{
+		red:   []uint8{},
+		green: []uint8{},
+		blue:  []uint8{},
+	}
+
+	segmentSize := (math.Pi * 3) / float64(ledCount)
+	maxBrightness := float64(180)
+	piDivision := float64(2)
+
+	for i := math.Pi * -1; i <= math.Pi*2; i += segmentSize {
+		red := math.Sin(i/piDivision + math.Pi/2)
+		blue := math.Sin(i / piDivision)
+		floats.red = SinAppend(floats.red, red, maxBrightness)
+		floats.blue = SinAppend(floats.blue, blue, maxBrightness)
+		if i <= 0 {
+			green := math.Sin(i/piDivision + math.Pi)
+			floats.green = SinAppend(floats.green, green, maxBrightness)
+		} else {
+			green := math.Sin(i/piDivision - math.Pi/2)
+			floats.green = SinAppend(floats.green, green, maxBrightness)
+		}
+	}
+
+	return floats
+}
+
+func rainbowCosCycle(floats RGB, seed int) error {
+	for i := 0; i < count; i++ {
+		ws2811.SetLed(i, RainbowCosColor(floats, i+seed))
+	}
+	err := ws2811.Render()
+	if err != nil {
+		ws2811.Clear()
+		return err
+	}
+	time.Sleep(5 * time.Millisecond)
+	return nil
 }
 
 func rainbowCycle(angle int) error {
@@ -59,6 +135,21 @@ func colorWipe(color uint32) error {
 
 		time.Sleep(5 * time.Millisecond)
 	}
+
+	return nil
+}
+
+func colorFlash(color uint32) error {
+	for i := 0; i < count; i++ {
+		ws2811.SetLed(i, color)
+	}
+	err := ws2811.Render()
+	if err != nil {
+		ws2811.Clear()
+		return err
+	}
+
+	time.Sleep(5 * time.Second)
 
 	return nil
 }
